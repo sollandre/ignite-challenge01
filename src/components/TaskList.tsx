34c79acba0b777613f05c9a5084ce1a20c778b74
Andre Solla <andrebmsolla@gmail.com>
2021-11-09 17:55:02 -0300

    Solution added

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!!newTaskTitle){
+      const newTask = {
+        id: Math.floor((Math.random()*900))+100,
+        title: newTaskTitle,
+        isComplete: false,
+      }
+      setNewTaskTitle("");
+      setTasks([...tasks, newTask]);
+    }
+    else{
+      alert("Um titulo é obrigatório para adicionar um novo to-do")
+    } 
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => {
+      if(task.id === id){
+        task.isComplete = !task.isComplete;
+      }
+      return task;
+    }));
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
